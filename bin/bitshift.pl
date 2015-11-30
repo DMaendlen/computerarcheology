575321f1c03f6cef1e16d17cb7d5b827648b5847
David Mändlen <david.maendlen@bifroe.st>
2015-11-30 19:01:01 +0100

    finalised pps

---
+++ bin/bitshift.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+# find out how bit shifting works
+
+use strict;
+use warnings;
+
+my $tty_nr = 34816;
+my $minor_mask = 0xFFFF00FF;
+my $major_mask = 0xFF00;
+
+my $major_nr = $tty_nr & $major_mask;
+$major_nr = $major_nr >> 8;
+my $minor_nr = $tty_nr & $minor_mask;
+
+print("major: $major_nr\n");
+print("minor: $minor_nr\n");
