50388abf41b2c24edef14a0b2771e7a44d53fd2d
David Mändlen <david.maendlen@bifroe.st>
2016-01-05 16:59:39 +0100

    trying to automate whole setup-process

--- bin/passwdToLdif.pl
+++ bin/passwdToLdif.pl
@@ -3,21 +3,37 @@
 use strict;
 use warnings;
 
-my $filename = "passwd.bsp";
+my $filename = $ARGV[0];
 
 open INFILE, $filename or do{
 	warn "Couldn't open $filename: $!";
 	return {};
 };
+
+unless ( -d "passwd" ) {
+	mkdir "passwd" or die "Error creating passwd-dir: $!";
+};
+
+unless ( -d "ldif" ) {
+	mkdir "ldif" or die "Error creating ldif-dir: $!";
+};
+
 while (my $line = <INFILE>) {
 	chomp($line);
 	my ($uid, $unused) = split(/:/, $line, 2);
-	open(OUTFILE, '>', "passwd.$uid") or do {
+	open(OUTFILE, '>', "passwd/passwd.$uid") or do {
 		warn "Couldn't open $filename: $!";
 		return {};
 	};
 	print(OUTFILE $line);
 	close(OUTFILE);
 }
+close(INFILE);
 
-close(INFILE)
+opendir(PASSWD, "passwd") or die "Can't open passwd: $!";
+while (my $file = readdir PASSWD) {
+	my ($unused, $uid) = split(/\./, $file);
+	`/usr/share/openldap/migration/migrate_passwd.pl $file "ldif/$uid.ldif"`
+}
+closedir PASSWD;
+
